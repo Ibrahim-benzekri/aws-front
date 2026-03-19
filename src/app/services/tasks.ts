ad9f8ca1da638dbd89834871e1593cc5bbee9fd2
ibrahim-benzekri <ibrahimbenzekri90@gmail.com>
2026-03-19 17:31:48 +0100

    api-gatewau

--- src/app/services/tasks.ts
+++ src/app/services/tasks.ts
@@ -10,12 +10,12 @@ import { Task } from '../../models/task';
 export class TaskService {
   private http = inject(HttpClient);
   private apiUrl2 = 'http://localhost:8080/tasks';
-  private apiUrl = 'http://34.200.246.127:8080/tasks';
+  private getapiUrl = 'https://2guks68ge9.execute-api.us-east-1.amazonaws.com/ender/proxy';
   getTasks(): Observable<Task[]> {
-    return this.http.get<Task[]>(this.apiUrl);
+    return this.http.get<Task[]>(this.getapiUrl);
   }
 
   createTask(task: { title: string }): Observable<Task> {
-    return this.http.post<Task>(this.apiUrl, task);
+    return this.http.post<Task>(this.getapiUrl, task);
   }
 }
